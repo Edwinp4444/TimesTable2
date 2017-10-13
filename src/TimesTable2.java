ec7b6a5f6437c13488ba24f4df71364e123641e6
Edwinp4444 <edwin.philogene@ocsbstudent.ca>
2017-10-13 14:39:37 -0400

    hiii

---
+++ src/TimesTable2.java
@@ -0,0 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+public class TimesTable2 {
+
+	public static void main(String[] agrs) {
+		// TODO Auto-generated method stub
+		Scanner in=new Scanner(System.in);
+		Random rnd=new Random();
+
+		//Setup Variables
+		int hi;
+		int low;
+		int num;
+		
+		
