6da48a08042456e6f2c505a22b741f277bb06c6f
Edwinp4444 <edwin.philogene@ocsbstudent.ca>
2017-10-16 13:24:19 -0400

    in

--- src/TimesTable2.java
+++ src/TimesTable2.java
@@ -13,3 +13,32 @@ public class TimesTable2 {
 		int num;
 		
 		
+		//Ask for hi, low and number
+		System.out.println("Welcome to the Times Table Challenge number 2!");
+		System.out.println("What is the biggest number you want?");
+		hi=in.nextInt();
+		System.out.println("What is the smallest number you want?");
+		low=in.nextInt();
+		
+		
+			
+		//Loop and ask questions or not 
+		for (int x=low; x<=hi; x++) {
+			System.out.print("\t");
+			System.out.print(x);
+		}
+		System.out.println();
+		
+		for (int x=low; x<=hi; x++) {
+			System.out.print(x);
+			System.out.print("\t");
+			
+			for (int y=low; y<=hi; y++) {
+				System.out.print(y*x);
+				System.out.print("\t");
+			}
+			
+			System.out.println();
+		}
+	}
+}
